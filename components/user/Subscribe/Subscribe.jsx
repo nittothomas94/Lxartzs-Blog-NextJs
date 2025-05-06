f9c685353de23444d440684a82d792d578add757
nittothomas <nittothomas94@example.com>
2025-05-06 17:04:57 +0530

    updated subscribe component

--- components/user/Subscribe/Subscribe.jsx
+++ components/user/Subscribe/Subscribe.jsx
@@ -1,7 +1,7 @@
 import EnterYourEmail from '../EnterYourEmail/EnterYourEmail';
 const Subscribe = () => {
   return (
-    <div className="w-full min-h-30  flex flex-col items-center justify-between gap-4 py-10 md:flex-row">
+    <div className="w-full min-h-30  flex flex-col items-center justify-between gap-4 py-10 md:flex-row md-px-10">
       <div className="w-full flex flex-col gap-4 items-center  md:w-1/2 lg:w-[500px] lg:items-start">
         <h1 className="font-bold  text-[27px] w-[400px] lg:text-[42px]">
           Subscribe to my <br className="hidden md:block" />
